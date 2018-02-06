7a152436f560b149230cd6ea1908cc858b38974c
Rohan Gupta <rohangupta@wustl.edu>
2018-02-06 02:50:56 -0600

    2. Critical values

--- problemset2.R
+++ problemset2.R
@@ -37,3 +37,61 @@ benford=function(x,m=TRUE,d=TRUE){ #Function takes as input (i) vector of electi
 }
 
 #2.
+#Function that will output table containing name of statistic, statistic as it was calculated, relevant number of asterisks, legend explaining asterisks
+print.benfords=function(x){ #Function takes as input vector of election returns
+  for(i in 1:length(x)){ #For each election return, find first significant digit
+    x[i]<-substr(x[i],1,1)
+  }
+  x<-as.numeric(x) #Change type from character to numeric
+  y<-NULL #Create vector of length 9 with values 0
+  for(i in 1:9){
+    y<-c(y,0)
+  }
+  for(i in 1:length(x)){ #For each first significant digit, add 1 to observed frequency
+    y[x[i]]<-y[x[i]]+1
+  }
+  z<-y/length(x) #Observed proportional frequency = observed frequency/observed vote total (vector with full digit distribution)
+  for(i in 1:length(y)){ #Calculate same expression in m and d
+    y[i]<-y[i]/length(x)-log10(1+1/i)
+  }
+  leemis<-max(y) #m is max of this value
+  chogains<-sqrt(sum(y^2)) #d is square root of sum of each value squared
+  output<-table(c("Leemis'","Cho-Gains'","Legend"),c("Name","Statistic","Significance")) #Create output table
+  output[1,1]<-"d" #Name of Cho-Gains' d statistic
+  output[2,1]<-"m" #Name of Leemis' m statistic
+  output[1,3]<-chogains #Cho-Gains' d statistic calculated
+  output[2,3]<-leemis #Leemis' m statistic calculated
+  legend<-"Signif. codes: 0.01 '***' 0.05 '**' 0.1 '*' 1" #Legend explaining asterisks
+  output[3,1]<-substr(legend,1,15)
+  output[3,2]<-substr(legend,16,30)
+  output[3,3]<-substr(legend,31,45)
+  a<-"" #Relevant number of asterisks
+  b<-""
+  if(chogains>=1.212){
+    a<-"*" #One star for significance at a = .10 level
+  }
+  if(chogains>=1.330){
+    a<-"**" #Two stars for significance at a = .05 level
+  }
+  if(chogains>=1.569){
+    a<-"***" #Three stars for significance at a = .01 level
+  }
+  if(leemis>=.851){
+    b<-"*" #One star for significance at a = .10 level
+  }
+  if(leemis>=.967){
+    b<-"**" #Two stars for significance at a = .05 level
+  }
+  if(leemis>=1.212){
+    b<-"***" #Three stars for significance at a = .01 level
+  }
+  output[1,2]<-a
+  output[2,2]<-b
+  return(output) #Output table
+}
+
+#Function that uses print.benfords() to create csv containing table in directory provided as argument to function
+sink.benfords=function(x,csv){
+  print.benfords(x)
+  sink(csv)
+}
